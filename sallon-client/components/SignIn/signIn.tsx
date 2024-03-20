93fc32b3bd2d8ad78cf9000c7f79671fd8c13b7d
Moyasi Ginko <mrsiyam61@gmail.com>
2024-03-20 07:19:45 +0600

    update design

--- sallon-client/components/SignIn/signIn.tsx
+++ sallon-client/components/SignIn/signIn.tsx
@@ -4,6 +4,9 @@ import React, { useState, FormEvent } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
+import logoImage from "../../assets/logo.jpg"; // Import your logo image here
+import "../../styles/login-register.css"; // Import the CSS file for styling
+import Image from "next/image";
 
 const SignIn = () => {
   const router = useRouter();
@@ -12,90 +15,94 @@ const SignIn = () => {
     password: "",
   });
   const { email, password } = formData;
-  const [errors, setError] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!email || !password) {
-      setError("All fields are required");
-    } else {
-      setError("");
-      try {
-        const req = await axios.post(
-          "http://localhost:3000/api/users/login",
-          formData
+    try {
+      const req = await axios.post(
+        "http://localhost:3000/api/users/login",
+        formData
+      );
+      if (req.status === 200) {
+        const response = req.data;
+        localStorage.setItem("name", JSON.stringify(response.name));
+        localStorage.setItem("email", JSON.stringify(response.email));
+        localStorage.setItem(
+          "accessToken",
+          JSON.stringify(response.accessToken)
         );
-        if (req.status === 200) {
-          const response = req.data;
-          localStorage.setItem("name", JSON.stringify(response.name));
-          localStorage.setItem("email", JSON.stringify(response.email));
-          localStorage.setItem(
-            "accessToken",
-            JSON.stringify(response.accessToken)
-          );
 
-          const cookieName = "refreshToken";
-          const cookieValue = response.refreshToken;
-          const expirationDate = new Date();
-          expirationDate.setMonth(expirationDate.getMonth() + 1);
-          document.cookie = `${cookieName}=${cookieValue}; expires=${expirationDate.toUTCString()}; path=/; SameSite=strict`;
-          router.push("/profile");
-          toast.success("Login successful");
-        }
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error) && error.response) {
-          if (
-            error.response.status === 401 &&
-            error.response.data.error === "Invalid Credintails"
-          ) {
-            setError("Email or password is incorrect");
-          } else {
-            console.log("Error: ", error);
-            setError("An Error has occurred");
-          }
+        const cookieName = "refreshToken";
+        const cookieValue = response.refreshToken;
+        const expirationDate = new Date();
+        expirationDate.setMonth(expirationDate.getMonth() + 1);
+        document.cookie = `${cookieName}=${cookieValue}; expires=${expirationDate.toUTCString()}; path=/; SameSite=strict`;
+        router.push("/profile");
+        toast.success("Login successful");
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        if (
+          error.response.status === 401 &&
+          error.response.data.error === "Invalid Credentials"
+        ) {
+          toast.error("Email or password is incorrect");
         } else {
-          console.log("Error:", error);
-          setError("An unKnown error occurred");
+          console.log("Error: ", error);
+          toast.error("An Error has occurred");
         }
+      } else {
+        console.log("Error:", error);
+        toast.error("An unKnown error occurred");
       }
     }
   };
+
   return (
-    <>
-      <h1 className="text-center pt-5 text-2xl mt-32">Sign in</h1>
-      <div className="text-red-700 text-center">{errors}</div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-2 items-center mt-10"
-      >
-        <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            className="p-1 text-red-900"
-            value={email}
-            onChange={handleChange}
-          />
+    <div className="login-container">
+      <div className="login-card">
+        <div className="picture-section"></div>
+        <div className="login-content">
+          <div className="logo-card-header">
+            <Image src={logoImage} alt="Logo" className="logo-card" />
+            <span className="login-card-text">Sign in</span>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                className="p-1 text-red-900"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                className="p-1 text-red-900"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="text-center mt-2">
+              <input type="submit" value="Submit" className="submit-btn" />
+            </div>
+          </form>
+          <div>
+            <p>
+              Dont have an account? <a href="/signup">Sign Up</a>
+            </p>
+          </div>
         </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="p-1 text-red-900"
-            value={password}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="text-center mt-2">
-          <input type="submit" value="submit" className="bg-red-900 p-2" />
-        </div>
-      </form>
-    </>
+      </div>
+    </div>
   );
 };
 
--- sallon-client/components/SignUp/signup.tsx
+++ sallon-client/components/SignUp/signup.tsx
@@ -1,9 +1,12 @@
-'use client'
+"use client";
 
-import React, { FormEvent, useState } from 'react';
-import axios, {AxiosError, AxiosResponse } from 'axios';
-import { useRouter } from 'next/navigation';
-import { toast } from 'react-toastify';
+import React, { FormEvent, useState } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import logoImage from "../../assets/logo.jpg"; // Import your logo image here
+import "../../styles/login-register.css"; // Import the CSS file for styling
+import Image from "next/image";
 
 const SignUp = () => {
   const router = useRouter();
@@ -12,79 +15,138 @@ const SignUp = () => {
     firstName: "",
     lastName: "",
     password: "",
-    cPassword: ""
-  })
-  const [error, setError] = useState("")
+    cPassword: "",
+  });
+  const [error, setError] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value })
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   const { email, firstName, lastName, password, cPassword } = formData;
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!email || !firstName || !lastName || !password || !cPassword) {
-      setError("All fields are required")
-    } else if (password !== cPassword) {
-      setError("Passwords do not match");
-    }
-    else {
-      setError("")
-      try {
-        const req: AxiosResponse<any> = await axios.post("http://localhost:3000/api/users/register", formData)
-        if (req.status === 201) {
-          router.push('/signin')
-          toast.success('Registration successful');
-        }
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)){
-          const axiosError = error as AxiosError;
-          if (axiosError.response){
-            const responseData = axiosError.response.data as {error : string}
-            if (axiosError.response.status === 400 && responseData.error=== "User already exists"){
-              setError("User already exists")
-            } else {
-              setError("An error occurred")
-            }
-          } else {
-            console.log('Error:', axiosError.message)
-            setError('Network error. Please try again later.')
-          }
 
+    // Check if any field is empty
+    if (!email || !firstName || !lastName || !password || !cPassword) {
+      toast.error("All fields are required");
+      return;
+    }
+
+    // Check if passwords match
+    if (password !== cPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+
+    setError(""); // Clear any previous error
+
+    try {
+      const req: AxiosResponse<any> = await axios.post(
+        "http://localhost:3000/api/users/register",
+        formData
+      );
+      if (req.status === 201) {
+        router.push("/signin");
+        toast.success("Registration successful");
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError;
+        if (axiosError.response) {
+          const responseData = axiosError.response.data as { error: string };
+          if (
+            axiosError.response.status === 400 &&
+            responseData.error === "User already exists"
+          ) {
+            setError("User already exists");
+          } else {
+            setError("An error occurred");
+          }
         } else {
-          console.log('Unknown error: ', error);
-          setError('An unknown error occurred.')
+          console.log("Error:", axiosError.message);
+          setError("Network error. Please try again later.");
         }
+      } else {
+        console.log("Unknown error: ", error);
+        setError("An unknown error occurred.");
       }
     }
-  }
-  return (
-    <>
-      <h1 className='text-center pt-5 text-2xl mt-16'>Signup</h1>
-      <div className='text-red-700 text-center '>{error}</div>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-2 items-center mt-10'>
-        <div>
-          <input type="text" name='email' value={email} placeholder='Email' className='p-1 text-red-900' onChange={handleChange} />
-        </div>
-        <div>
-          <input type="text" name='firstName' placeholder='First Name' className='p-1 text-red-900' value={firstName} onChange={handleChange} />
-        </div>
-        <div>
-          <input type="text" name='lastName' placeholder='Last Name' className='p-1 text-red-900' value={lastName} onChange={handleChange} />
-        </div>
-        <div>
-          <input type="password" name='password' placeholder='Password' className='p-1 text-red-900' value={password} onChange={handleChange} />
-        </div>
-        <div>
-          <input type="password" name='cPassword' placeholder='Confirm Password' className='p-1 text-red-900' value={cPassword} onChange={handleChange} />
-        </div>
-        <div className='text-center mt-2'>
-          <input type="submit" value='submit' className='bg-red-900 p-2' />
-        </div>
-      </form>
-    </>
+  };
 
-  )
-}
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <div className="picture-section"></div>
+        <div className="login-content">
+          <div className="logo-card-header">
+            <Image src={logoImage} alt="Logo" className="logo-card" />
+            <span className="login-card-text">Sign up</span>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                className="p-1 text-red-900"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                className="p-1 text-red-900"
+                value={firstName}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                className="p-1 text-red-900"
+                value={lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="p-1 text-red-900"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="cPassword"
+                placeholder="Confirm Password"
+                className="p-1 text-red-900"
+                value={cPassword}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="text-center mt-2">
+              <input type="submit" value="Submit" className="submit-btn" />
+            </div>
+          </form>
+          <div>
+            <p>
+              Dont have an account? <a href="/signin">Sign In</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SignUp;
